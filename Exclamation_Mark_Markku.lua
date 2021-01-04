2f0308751878710a7158fb890205ef6f7f53de1a
Kimmo <kimmoturunen@gmail.com>
2021-01-04 15:06:15 +0200

    init

--- Exclamation_Mark_Markku.lua
+++ Exclamation_Mark_Markku.lua
@@ -1,1 +1,24 @@
-print ("R.I.P Markku Uula Aleksi Laiho 4.1.2021")
+local markkuFacts = {
+    'Markku Uula Aleksi "Alexi "Wildchild"" Laiho oli ensimmäinen suomalainen kitaristi, josta on tehty artikkeli arvostettuun Guitar World -lehteen',
+    'Markku Uula Aleksi "Alexi "Wildchild"" on myös valittu Guitar World -lehden vuoden 2009 lukijaäänestyksessä parhaaksi metallikitaristiksi. Samassa äänestyksessä taakse jäivät muun muassa Kirk Hammett, Slash ja Zakk Wylde.'
+}
+
+local markkuFrame = CreateFrame("FRAME", "MarkkuFrame");
+markkuFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
+local function eventHandler(self, event, ...)
+    print ("R.I.P Markku Uula Aleksi Laiho 4.1.2021")
+end
+markkuFrame:SetScript("OnEvent", eventHandler);
+
+local captureFrame = CreateFrame("FRAME", "MarkkuCaptureFrame")
+captureFrame:RegisterEvent("CHAT_MSG_SAY")
+captureFrame:SetScript("OnEvent", function(self, event, msg)
+    if msg == "!markku" then
+        print(getRandomMarkkuFact())
+        -- SendChatMessage("Lol!", "SAY")
+    end
+end)
+
+function getRandomMarkkuFact ()
+    print( markkuFacts[ math.random( #markkuFacts ) ] )
+end
